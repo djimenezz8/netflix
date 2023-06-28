f95bf2c8ccf0d94738a054eb13a71c093455036e
djimenezz8 <diegojimenezsm24@gmail.com>
2023-06-28 01:25:47 -0400

    Cambio de base de datos
    Cambio de nombre, usuario y contraseña

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'netflix' );
+define( 'DB_NAME', 'diegjimenezs_netflix' );
 
 /** Database username */
-define( 'DB_USER', 'netflix' );
+define( 'DB_USER', 'diegjimenezs_netflix' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'netflix' );
+define( 'DB_PASSWORD', 'diegjimenezs_netflix' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
